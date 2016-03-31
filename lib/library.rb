163c879a734e591ebbeda29a547556fcb190fa9f
Shawn Michaud <shawnmne@hotmail.com>
2016-03-31 09:39:38 -0500

    added library model

---
+++ lib/library.rb
@@ -0,0 +1,12 @@
+#this is a class that creates a library object
+
+#+branch_name - string: name of the library
+#+address - string: address of the library
+#+phone - string: phone # of library
+
+class Library < ActiveRecord::Base
+
+
+
+
+end

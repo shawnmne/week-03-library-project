9b78a971d3674926e233656b9e7476907e4867be
Shawn Michaud <shawnmne@hotmail.com>
2016-03-31 19:26:33 -0500

    index built for library

--- main.rb
+++ main.rb
@@ -7,6 +7,33 @@ require_relative "lib/staff_member.rb"
 require_relative "lib/book.rb"
 require_relative "lib/patron.rb"
 
+def make_selection
+	print "Please make a selection "
+	gets.chomp.to_i
+end
+
+#show_all_libraries method shows all the libraries
+#
+#
+#
+#
+def show_all_libraries
+		puts "\nAvailable Libraries\n"
+		Library.all.each_with_index do  |library, index|
+			puts "#{index}  #{library.branch_name}"
+		end
+end
+
+#select_choice_library method allows the user to select 
+#   a chice from a menu in the library directory
+#
+#
+#
+#
+def select_choice_library
+	puts "\n1 Add a Library\n2 Examine a Library\n3 Edit a Library\n4 Remove a Library\n5 View All Libraries\n6 View Staff Members\n0 Return to Main Menu"
+	select = make_selection
+end
 
 #main_menu method allows a user to select between 5 different
 #   options - library, patron, book, staff member, and exit
@@ -15,11 +42,11 @@ require_relative "lib/patron.rb"
 #select is returned
 def main_menu
 	puts "\n1 Library\n2 Staff Members\n3 Books\n4 Patrons\n0 Exit\n"
-	print"Please make a selection: "
-	select = gets.chomp.to_i
+	select = make_selection
 
 	if select == 1
-#		library
+		show_all_libraries
+		select_choice_library
 
  	elsif select == 2
 # 	staff member
@@ -52,7 +79,7 @@ while choice != 0
 	choice = main_menu
 end	
 
-binding.pry	
+#binding.pry	
  
 
 
